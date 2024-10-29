0accd32ec709606bae6209e58a7ad3eb226c4163
Romihub <learncloud16@gmail.com>
2024-10-29 14:17:15 +0000

    Initial commit

---
+++ Jenkins-Server-TF/backend.tf
@@ -0,0 +1,16 @@
+terraform {
+  backend "s3" {
+    bucket         = "my-eksaws-basket1"
+    region         = "us-east-1"
+    key            = "End-to-End-Kubernetes-Three-Tier-Project/Jenkins-Server-TF/terraform.tfstate"
+    dynamodb_table = "lock-terra"
+    encrypt        = true
+  }
+  required_version = ">=0.13.0"
+  required_providers {
+    aws = {
+      version = ">= 2.7.0"
+      source  = "hashicorp/aws"
+    }
+  }
+}
